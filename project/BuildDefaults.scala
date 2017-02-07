adb25db9a533ae58a3b38985f0b6aac5ec13af18
radekg <radek@gruchalski.com>
2017-02-07 19:16:18 +0100

    Only use scoverage when in Test. This removes the dependency on the runtime.

--- project/BuildDefaults.scala
+++ project/BuildDefaults.scala
@@ -1,7 +1,7 @@
 object BuildDefaults {
 
   def buildScalaVersion = sys.props.getOrElse("scala.version", "2.11.8")
-  def buildVersion = "0.2.1"
+  def buildVersion = "0.2.2"
   def buildOrganization = "uk.co.appministry"
 
 }
