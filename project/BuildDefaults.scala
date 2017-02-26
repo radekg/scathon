482f3cf59a1ced2967e46c6dd0c38c1119f90dd9
radekg <radek@gruchalski.com>
2017-02-26 21:09:55 +0100

    0.2.4 snapshot

--- project/BuildDefaults.scala
+++ project/BuildDefaults.scala
@@ -1,7 +1,7 @@
 object BuildDefaults {
 
   def buildScalaVersion = sys.props.getOrElse("scala.version", "2.11.8")
-  def buildVersion = "0.2.3"
+  def buildVersion = "0.2.4-snapshot"
   def buildOrganization = "uk.co.appministry"
 
 }
